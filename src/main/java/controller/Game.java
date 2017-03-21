3181675ee3a6efcc49d07171780856972d9a2bb2
sonumax <sonumax2@gmail.com>
2017-03-21 23:47:03 +0300

    Check valid coordinate x and y

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -4,11 +4,15 @@ import model.Player;
 
 public class Game {
 
+    public static final int MIN_COORDINATE = 0;
+
+    public static final int MAX_COORDINATE = 2;
+
     private final String gameName;
 
     private Player[] players;
 
-    public Game(final String gameName) {
+    public Game(String gameName) {
         this.gameName = gameName;
     }
 
@@ -17,7 +21,13 @@ public class Game {
     }
 
     public boolean move(final int x, final int y) {
-        return false;
+        if (!checkCoordinate(x) || !checkCoordinate(y)) {
+            return false;
+        }
+
+        //TODO
+
+        return true;
     }
 
     public String getGameName() {
@@ -26,5 +36,13 @@ public class Game {
 
     public Player[] getPlayers() {
         return players;
+    }
+
+    private static boolean checkCoordinate(final int coordinate) {
+        if (coordinate < MIN_COORDINATE || coordinate > MAX_COORDINATE) {
+            return false;
+        }
+
+        return true;
     }
 }
