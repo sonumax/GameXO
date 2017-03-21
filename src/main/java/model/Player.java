e5c4164e00abf02d1a66ec1ab22c6b5f390751c8
sonumax <sonumax2@gmail.com>
2017-03-21 23:22:39 +0300

    Add Player

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Player {
+
+    private final String name;
+
+    private final String figure;
+
+    public Player(final String name, final String figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFigure() {
+        return figure;
+    }
+}
