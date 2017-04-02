4efc9599ae88a6bcbf262e98b5a90b97ad43a114
sonumax <sonumax2@gmail.com>
2017-04-02 13:51:15 +0300

    Now figure is enum

--- src/main/java/model/Figure.java
+++ src/main/java/model/Figure.java
@@ -1,22 +1,5 @@
 package model;
 
-public class Figure {
-
-    private final String figure;
-
-    public Figure(String figure) {
-
-        assert figure != null;
-
-        this.figure = figure;
-    }
-
-    public String getFigure() {
-        return this.figure;
-    }
-
-    @Override
-    public String toString() {
-        return this.figure;
-    }
+public enum  Figure {
+    X, O
 }
