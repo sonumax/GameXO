352f8d2d0e3d60a7fb2a2039cbb5c3567f4443a4
sonumax <sonumax2@gmail.com>
2017-04-02 13:50:07 +0300

    Add checkCoordinates and setFigure

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,21 +1,51 @@
 package model;
 
-import helpers.CoordinateHelper;
+
+import exceptions.InvalidCoordinateException;
 
 public class Board {
+    public static final int MIN_COORDINATE = 0;
 
-    public static final int SIZE_FIELD = 3;
+    private Figure[][] figures;
 
-    private Figure[][] figures = new Figure[SIZE_FIELD][SIZE_FIELD];
-
-    public void initArray() {
-        //TODO
+    public Board(final int sizeFieldX, final int sizeFieldY) {
+        figures = new Figure[sizeFieldX][sizeFieldY];
     }
 
-    public Figure getFigure(final int x, final int y) {
-        if(!CoordinateHelper.checkCoordinate(x) || !CoordinateHelper.checkCoordinate(y)) {
+    public Figure getFigure(final Point point ) {
+        if(!checkCoordinateX(point.getX()) || !checkCoordinateY(point.getY())) {
             return null;
         }
-        return figures[x][y];
+        return figures[point.getX()][point.getY()];
+    }
+
+    public void setFigures(final Figure figure , final Point point) throws InvalidCoordinateException {
+        if (!checkCoordinateX(point.getY()) || !checkCoordinateY(point.getY())) {
+            throw new InvalidCoordinateException();
+        }
+
+        figures[point.getX()][point.getY()] = figure;
+    }
+
+    public boolean checkCoordinateX(final int coordinateX) {
+        if (coordinateX < MIN_COORDINATE || coordinateX > figures.length) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkCoordinateY(final int coordinateY) {
+        if (coordinateY < MIN_COORDINATE || coordinateY > figures[0].length) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getSizeFieldX() {
+        return figures.length;
+    }
+
+    public int getSizeFieldY() {
+        return figures[0].length;
     }
 }
