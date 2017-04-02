54120e5fbfe679a15fc2748b48ee356ef9287f1a
sonumax <sonumax2@gmail.com>
2017-04-02 13:56:12 +0300

    Created Test for MoveController class

---
+++ src/test/java/controller/MoveControllerTest.java
@@ -0,0 +1,61 @@
+package controller;
+
+import exceptions.AlreadyOccupiedException;
+import exceptions.InvalidCoordinateException;
+import exceptions.InvalidMoveException;
+import model.Board;
+import model.Figure;
+import model.Point;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.fail;
+
+public class MoveControllerTest {
+
+    @Test
+    public void testApplyFigure() {
+        final Board board = new Board(3, 3);
+        final Figure inputFigure = Figure.O;
+        final Point inputPoint = new Point(0, 0);
+
+        MoveController moveController = new MoveController();
+
+        try {
+            moveController.applyFigure(board, inputFigure, inputPoint);
+        } catch (InvalidMoveException | AlreadyOccupiedException | InvalidCoordinateException e) {
+            e.printStackTrace();
+        }
+
+        Figure actualFigure = board.getFigure(inputPoint);
+
+        assertEquals(inputFigure, actualFigure);
+    }
+
+    @Test
+    public void testApplyFigureIsNotSet() {
+        final Board board = new Board(3, 3);
+        final Point inputPoint = new Point(0, 0);
+
+        Figure actualFigure = board.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void testApplyFigureWhenPointOccupied() {
+        final Board board = new Board(3, 3);
+        final Figure inputFigure = Figure.O;
+        final Point inputPoint = new Point(0, 0);
+
+        MoveController moveController = new MoveController();
+
+        try {
+            board.setFigures(Figure.X, inputPoint);
+            moveController.applyFigure(board, inputFigure, inputPoint);
+            fail();
+        } catch (InvalidMoveException | AlreadyOccupiedException | InvalidCoordinateException e) {
+        }
+    }
+}
