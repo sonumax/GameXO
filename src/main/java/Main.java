34369645dc4a93cf80082cd2a97be6fc48b9b855
sonumax <sonumax2@gmail.com>
2017-05-08 23:49:56 +0300

    Crater Main class with main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
+import controller.GameController;
+import model.Board;
+import model.Figure;
+import model.Player;
+import view.ConsoleView;
+
 public class Main {
+
+    public static final int SIZE_FIELD = 3;
+
+    public static void main(String[] args) {
+        final Player max = new Player("Max", Figure.X);
+        final Player vlad = new Player("Vlad", Figure.O);
+        Player[] players = {max, vlad};
+        Board board = new Board(SIZE_FIELD);
+        GameController gameController = new GameController("XO", board, players);
+        ConsoleView view = new ConsoleView(gameController);
+
+        view.showGameNames();
+        view.showPlayersName();
+        view.showBoard();
+        while (view.move()) {
+            view.showBoard();
+        }
+    }
 }
